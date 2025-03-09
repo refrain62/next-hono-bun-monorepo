3ea27a279224415de158da9bacd25e70b0f8b578
refrain62 <nao_h13@hotmail.com>
2025-03-09 14:20:05 +0900

    フロントとバックエンドの整備し、DBと連携

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,8 +2,15 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { zValidator } from '@hono/zod-validator'
 import { z } from 'zod'
+import { todosTable } from './db/schema'
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
 
-const app = new Hono()
+export type Env = {
+  DATABASE_URL: string;
+};
+
+const app = new Hono<{ Bindings: Env }>();
 
 app.use('*', cors({
   origin: '*'
@@ -11,7 +18,7 @@ app.use('*', cors({
 
 const todoSchema = z.object({
   title: z.string().min(2),
-  description: z.string().nullable()
+  description: z.string().nullable(),
 })
 
 const route = app
@@ -23,11 +30,22 @@ const route = app
       return c.text(result.error.issues[0].message, 400)
     }
   }),
-    (c) => {
+    async (c) => {
       const { title, description } = c.req.valid('json')
-      return c.json({ title, description })
+      const client = postgres(c.env.DATABASE_URL, { prepare: false })
+      const db = drizzle({ client })
+      const todo = await db.insert(todosTable).values({ title, description }).returning()
+      return c.json({ todo: todo[0] })
+    })
+  .get('/todos', async (c) => {
+    const client = postgres(c.env.DATABASE_URL, { prepare: false })
+    const db = drizzle({ client })
+    const todos = await db.select().from(todosTable)
+    if (!todos) {
+      return c.text('Failed to fetch todos', 500)
+    }
+    return c.json({ todos })
   })
-
 export type AppType = typeof route
 
 export default app
